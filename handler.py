f16536df4edbfd93e0f2164f4e53889e7cbbac17
Socialechoagency <socialecho@web.de>
2025-12-30 02:59:59 +0100

    Fix syntax error in logger initialization

--- handler.py
+++ handler.py
@@ -15,7 +15,7 @@ import cv2
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__
+logger = logging.getLogger(__name__)
 
 # CUDA 검사 및 설정
 def check_cuda_availability():
